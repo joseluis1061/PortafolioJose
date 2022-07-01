8d40c9ced8ac49b7fd4eb77135eee74e4aeb25c2
jose <joseluis836@hotmail.com>
2022-07-01 18:30:28 -0500

    Se corrigio falla en cards ahora todas tienen el mismo ancho

--- src/initialState.js
+++ src/initialState.js
@@ -1,37 +1,37 @@
 /*Información de proyectos para el portafolio */
-import proyecto1 from './assets/img/laVeterinaria.png';
-import proyecto2 from './assets/img/batataBit.png';
-import proyecto3 from './assets/img/blogDeCafe.png';
-import proyecto4 from './assets/img/pageFreelancer.png';
+import laVeterinaria from './assets/img/laVeterinaria.png';
+import batataBit from './assets/img/batataBit.png';
+import blogDeCafe from './assets/img/blogDeCafe.png';
+import pageFreelancer from './assets/img/pageFreelancer.png';
 
 const initialState = [
   {
-    image: proyecto1, 
+    image: laVeterinaria, 
     title: 'La Veterinaria', 
     description: 'Oraginizador de citas veterinarias puedes agregar pacientes, sintomas y un contacto del paciente',
-    urlPage: proyecto1,
+    urlPage: 'https://laveterinaria.netlify.app/',
     urlCode: 'https://github.com/joseluis1061/citas_veterinaria'
   },
   {
-    image: proyecto2, 
-    title: 'Batata Bit', 
-    description: 'Landing Page con diseño para consultar el valor de criptomonedas en líena',
-    urlPage: 'https://laveterinaria.netlify.app/',
-    urlCode: 'https://github.com/joseluis1061/batataBit'
-  },
-  {
-    image: proyecto3, 
+    image: blogDeCafe, 
     title: 'Blog De Café', 
     description: 'Landing Page diseñada para los amantes de un buen café',
     urlPage: 'https://dulcet-mooncake-c8416a.netlify.app/',
    urlCode: 'https://github.com/joseluis1061/BlogDeCaffe'
   },
   {
-    image: proyecto4, 
+    image: pageFreelancer, 
     title: 'FreeLancerPage', 
     description: 'Landing Page diseñada para un freeLancer',
-    urlPage: 'https://dulcet-mooncake-c8416a.netlify.app/',
-   urlCode: 'https://github.com/joseluis1061/BlogDeCaffe'
+    urlPage: 'https://portafoliofrontendweb.netlify.app/',
+    urlCode: 'https://github.com/joseluis1061/sitioFrelancer'
+  },
+  {
+    image: batataBit, 
+    title: 'Batata Bit', 
+    description: 'Landing Page con diseño para consultar el valor de criptomonedas en líena',
+    urlPage: 'https://laveterinaria.netlify.app/',
+    urlCode: 'https://github.com/joseluis1061/batataBit'
   },
 
 ]
