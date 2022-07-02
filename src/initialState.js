bb04f6d2e3d54b4996f90f30bdeda886f06a2b60
jose <joseluis836@hotmail.com>
2022-07-02 10:10:52 -0500

    Se modificaron los textos con buena redacción y corrección de ortografia. Se agregaron los enlaces a redes sociales.

--- src/initialState.js
+++ src/initialState.js
@@ -8,21 +8,21 @@ const initialState = [
   {
     image: laVeterinaria, 
     title: 'La Veterinaria', 
-    description: 'Oraginizador de citas veterinarias puedes agregar pacientes, sintomas y un contacto del paciente',
+    description: 'Oraginizador de citas veterinarias puedes agregar pacientes, síntomas y un contacto del paciente',
     urlPage: 'https://laveterinaria.netlify.app/',
     urlCode: 'https://github.com/joseluis1061/citas_veterinaria'
   },
   {
     image: blogDeCafe, 
     title: 'Blog De Café', 
-    description: 'Landing Page diseñada para los amantes de un buen café',
+    description: 'Landing Page diseñada para los amantes de un buen café que quieran saber todos los secretos para su preparación',
     urlPage: 'https://dulcet-mooncake-c8416a.netlify.app/',
    urlCode: 'https://github.com/joseluis1061/BlogDeCaffe'
   },
   {
     image: pageFreelancer, 
     title: 'FreeLancerPage', 
-    description: 'Landing Page diseñada para un freeLancer',
+    description: 'Landing Page diseñada para un freeLancer facil de adaptar en diseño e información',
     urlPage: 'https://portafoliofrontendweb.netlify.app/',
     urlCode: 'https://github.com/joseluis1061/sitioFrelancer'
   },
@@ -30,7 +30,7 @@ const initialState = [
     image: batataBit, 
     title: 'Batata Bit', 
     description: 'Landing Page con diseño para consultar el valor de criptomonedas en líena',
-    urlPage: 'https://laveterinaria.netlify.app/',
+    urlPage: 'https://luminous-gingersnap-397aa2.netlify.app',
     urlCode: 'https://github.com/joseluis1061/batataBit'
   },
 
