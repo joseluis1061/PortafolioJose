bb6ef3ec22cc4d98599ee12291bc3178030fcc9d
jose <joseluis836@hotmail.com>
2022-07-19 15:50:34 -0500

    Se eliminaron lineas tipo consolo.log y se mejoro el mensaje de error

--- src/formValidation.js
+++ src/formValidation.js
@@ -1,5 +1,4 @@
 const formContacto = document.querySelector('.form__contacto');
-//formContacto.addEventListener('submit', formValidation);
 const inputNombre = document.querySelector('#name');
 const inputEmail = document.querySelector('#email');
 const inputMesage = document.querySelector('#textarea');
@@ -13,11 +12,9 @@ const formValidation= (event) =>{
   formInputs.forEach(formInput => {   
     if (formInput.value.trim() === "") {    
       formInput.style.border = "2px solid red";
-      //errorText.innerHTML = 'Todos los campos * son obligatorios';
       enviar = false;
     } else {
       formInput.style.border = "2px solid green";
-      //errorText.innerHTML = '';
       enviar = true;
     }
   });
@@ -29,32 +26,27 @@ const formValidation= (event) =>{
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: new URLSearchParams(formData).toString(),
     })
-      //.then(() => console.log("Form successfully submitted"))
-      //.catch((error) => alert(error));
 
-      .then(() => {
-        errorText.innerHTML = 'Enviado';
-        errorText.style.color = 'green';
-      })
-      .catch((error) => alert('Ha ocurrido un error con le mensaje. Intentalo de nuevo: '+error));   
+    .then(() => {
+      errorText.innerHTML = 'Tu mensaje ha sido enviado. Pronto te contactare';
+      errorText.style.color = 'green';
+    })
+    .catch((error) => alert('Ha ocurrido un error con le mensaje. Intentalo de nuevo: '+error));   
 
-      setTimeout(() =>{
-        errorText.innerHTML = '';
-        inputNombre.value = '';
-        inputEmail.value = '';
-        inputMesage.value = '';
-        inputNombre.style.border = "1px solid black";
-        inputEmail.style.border = "1px solid black";
-        inputMesage.style.border = "1px solid black";
-      }, 3000);
+    setTimeout(() =>{
+      errorText.innerHTML = '';
+      inputNombre.value = '';
+      inputEmail.value = '';
+      inputMesage.value = '';
+      inputNombre.style.border = "1px solid black";
+      inputEmail.style.border = "1px solid black";
+      inputMesage.style.border = "1px solid black";
+    }, 3000);
   }
   else if(enviar=== false){
     errorText.innerHTML = 'Todos los campos * son obligatorios';
     errorText.style.color = 'red';
   }
-
- 
-
 }
 
 export default formValidation;
