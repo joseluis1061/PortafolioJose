834726dea99ed64daa60c279d31bb295494da53a
jose <joseluis836@hotmail.com>
2022-07-19 13:15:57 -0500

    Se agregaron las validaciones para evitar el envio de formularios vacios se agregaron los mensajes para el usuario.

--- src/formValidation.js
+++ src/formValidation.js
@@ -13,26 +13,48 @@ const formValidation= (event) =>{
   formInputs.forEach(formInput => {   
     if (formInput.value.trim() === "") {    
       formInput.style.border = "2px solid red";
-      errorText.innerHTML = 'Todos los campos * son obligatorios';
+      //errorText.innerHTML = 'Todos los campos * son obligatorios';
       enviar = false;
     } else {
       formInput.style.border = "2px solid green";
-      errorText.innerHTML = '';
+      //errorText.innerHTML = '';
       enviar = true;
     }
   });
 
-  if(enviar){
+  if(enviar=== true){
     let formData = new FormData(formContacto);
     fetch("/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: new URLSearchParams(formData).toString(),
     })
-      .then(() => console.log("Form successfully submitted"))
-      .catch((error) => alert(error));
+      //.then(() => console.log("Form successfully submitted"))
+      //.catch((error) => alert(error));
 
-    }
+      .then(() => {
+        errorText.innerHTML = 'Enviado';
+        errorText.style.color = 'green';
+      })
+      .catch((error) => alert(error));   
+
+      setTimeout(() =>{
+        errorText.innerHTML = '';
+        inputNombre.value = '';
+        inputEmail.value = '';
+        inputMesage.value = '';
+        inputNombre.style.border = "1px solid black";
+        inputEmail.style.border = "1px solid black";
+        inputMesage.style.border = "1px solid black";
+      }, 3000);
+  }
+  else if(enviar=== false){
+    errorText.innerHTML = 'Todos los campos * son obligatorios';
+    errorText.style.color = 'red';
+  }
+
+ 
+
 }
 
 export default formValidation;
